9880e123af4ce7a32d25da747cffd8a8efd9ba70
Ali Souri <souri7205@gmail.com>
2026-06-25 16:42:44 +0330

    config project

--- src/app.js
+++ src/app.js
@@ -3,6 +3,12 @@ const app = express();
 
 const path = require('path');
 
+
+//static folders
+app.use('/css', express.static(path.join(__dirname, 'public', 'css')))
+
+
+
 //Template engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,'views'));
