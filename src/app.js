8997af4a0a3aa780a7f6ed326c4a53930a5eb455
Ali Souri <souri7205@gmail.com>
2026-06-25 16:21:27 +0330

    config project

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 
+const path = require('path');
 
-
+//Template engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname,'views'));
 
 
 module.exports = app;
