232c2b5182c417f5f6fde38bc4f8d9a8fe140a74
Ali Souri <souri7205@gmail.com>
2026-06-25 23:05:22 +0330

    config project

--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
 
+const {setHeaders} = require('./utils/middlewares/headers');
 const path = require('path');
 
+
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({extended: true, limit: '50mb'}));
+
+
+//cors policy
+app.use(setHeaders);
 
 //static folders
 app.use('/css', express.static(path.join(__dirname, 'public', 'css')));
@@ -19,6 +27,13 @@ app.set('views', path.join(__dirname,'views'));
 //routes
 app.get('/', (req, res) => {
     return res.render('index')
+});
+
+
+//404 error handler
+
+app.use((req, res) => {
+    return res.status(404).json({message: `this path is not found: ${req.path}`})
 });
 
 module.exports = app;
