9cd0c50ea9883f3d20a56100ae0f030c1571d4c2
Ali Souri <souri7205@gmail.com>
2026-06-27 11:59:32 +0330

    config project

--- src/server.js
+++ src/server.js
@@ -1,8 +1,21 @@
 const app = require('./app');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
+//load env
+const productionMode = process.env.NODE_ENV === 'production'
+if (!productionMode) {
+    dotenv.config()
+};
+
+async function connectToDb () {
+    await mongoose.connect(process.env.MONGO_URI, () => {
+
+    })
+};
 
 function startServer () {
-    app.listen(4002, () => {
+    app.listen(process.env.PORT, () => {
         console.log('server run on port 4002')
     })
 };
