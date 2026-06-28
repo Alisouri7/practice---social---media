d99edcefce7723b591c03a3451d8319291a05d06
Ali Souri <souri7205@gmail.com>
2026-06-28 12:21:43 +0330

    user model

---
+++ src/modules/users/model/User.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const { string } = require('yup');
+
+const schema = mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+        username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+        biography: {
+        type: String
+    },
+        name: {
+        type: String,
+    },
+        password: {
+        type: String,
+        required: true
+    },
+        profilePicture: {
+        type: String,
+        required: true
+    },
+        role: {
+        type: String,
+        enum: ['ADMIN', 'USER'],
+        default: 'USER'
+    },
+        private: {
+        type: Boolean,
+        default: false,
+    },
+        isVerified: {
+        type: Boolean,
+        default: false
+    }
+}, { timestamps: true});
+
+
+schema.pre('save', async (next) => {
+    try {
+        this.password = await bcrypt.hash(this.password, 10)
+        next()
+    } catch (error) {
+        next(error)
+    }
+});
+
+const model = mongoose.model('User', schema);
+
+module.exports = model;
