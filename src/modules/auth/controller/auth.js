a31a07ed7da3d69a04f9897cddd178b2ca6ffbfc
Ali Souri <souri7205@gmail.com>
2026-07-04 18:47:26 +0330

    login controller

--- src/modules/auth/controller/auth.js
+++ src/modules/auth/controller/auth.js
@@ -2,9 +2,11 @@ const { errorHandler } = require('./../../../utils/middlewares/errorHandler');
 const responseHandler = require('./../../../utils/responses');
 const userModel = require('./../../users/model/User');
 const refreshTokenModel = require('./../../token/refresh_token/model/refreshToken');
-const userRegisterValidationSchema  = require('./../../../utils/validators/registerUserValidator')
+const userRegisterValidationSchema = require('./../../../utils/validators/registerUserValidator')
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
 
 exports.showRegisterView = async (req, res) => {
     return res.render('./Pages/Auth/Register/index')
@@ -14,15 +16,15 @@ exports.showRegisterView = async (req, res) => {
 exports.register = async (req, res) => {
     try {
         const { email, username, password, name } = req.body;
-        
-        
+
+
         //Validation
         await userRegisterValidationSchema.validate({
             email,
             username,
             password,
             name
-        }, { 
+        }, {
             abortEarly: false                 //abortEarly: false => determines that validate all properties and then return an array of errors
         });
 
@@ -40,12 +42,12 @@ exports.register = async (req, res) => {
         if (isFirstUser) {
             role = 'ADMIN'
         };
-     
+
         const newUser = new userModel({ email, username, name, password, role });
-        
+
         await newUser.save();
-        
-        const accessToken = jwt.sign({userID: newUser._id}, process.env.JWT_SECRET, {
+
+        const accessToken = jwt.sign({ userID: newUser._id }, process.env.JWT_SECRET, {
             expiresIn: '30day'
         });
 
@@ -54,15 +56,57 @@ exports.register = async (req, res) => {
 
 
 
-        res.cookie('access-token', accessToken, {maxAge: 900_000_000, httpOnly: true});
-        res.cookie('refresh-token', refreshToken, {maxAge: 900_000_000, httpOnly: true});
+        res.cookie('access-token', accessToken, { maxAge: 900_000_000, httpOnly: true });
+        res.cookie('refresh-token', refreshToken, { maxAge: 900_000_000, httpOnly: true });
 
         req.flash('success', 'Registration Successfull')
 
         return res.redirect('/auth/register')
         // return responseHandler.successResponse(res, 201, { message: 'User created successfully', user: { ...newUser.toObject() , password: undefined } })
     } catch (error) {
-        responseHandler.errorResponse(res, 400, error,{error})
+        responseHandler.errorResponse(res, 400, error, { error })
         // next(error)
     }
 }
+
+exports.showLoginView = async (req, res) => {
+    return res.render('./Pages/Auth/Login/index')
+};
+
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email }).lean();
+
+    if (!user) {
+        req.flash('error', 'Invalid Email or Password')
+
+        return res.redirect('/auth/login')
+    }
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordMatch) {
+        req.flash('error', 'Invalid Email or Password')
+
+        return res.redirect('/auth/login')
+    };
+
+
+
+    const accessToken = jwt.sign({ userID: user._id }, process.env.JWT_SECRET, {
+        expiresIn: '30day'
+    });
+
+    const refreshToken = await refreshTokenModel.createToken(user);
+
+
+
+
+    res.cookie('access-token', accessToken, { maxAge: 900_000_000, httpOnly: true });
+    res.cookie('refresh-token', refreshToken, { maxAge: 900_000_000, httpOnly: true });
+
+    req.flash('success', 'Login Successfull')
+
+    return res.redirect('/auth/login')
+};
--- src/modules/auth/routes/auth.js
+++ src/modules/auth/routes/auth.js
@@ -6,5 +6,9 @@ router.route('/register')
     .get(authController.showRegisterView)
     .post(authController.register);
 
+router.route('/login')
+    .get(authController.showLoginView)
+    .post(authController.login);
+
 
 module.exports = router;
