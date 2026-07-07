caf02dfd519c5401ef219954a50f32d41e05cdd0
Ali Souri <souri7205@gmail.com>
2026-07-07 22:44:58 +0330

    uploader middleware

--- src/modules/posts/controller/post.js
+++ src/modules/posts/controller/post.js
@@ -15,6 +15,28 @@ exports.createPost = async (req, res, next) => {
             req.flash('error', 'Media is required')
             return res.render('./Pages/postUpload/index')
         }
+
+        await createPostValidator.validate({description}, {abortEarly: false});
+
+
+        //create new post
+        const mediaUrlPath = `/images/posts/${req.file.filename}`
+
+        const post = new postModel({
+            media: {
+                path: mediaUrlPath,
+                filename: req.file.filename
+            },
+            description,
+            hashtags: tags,
+            user: req.user._id
+        });
+
+        await post.save();
+        
+        req.flash('success', 'Post Created Successfully:)')
+        return res.render('./Pges/postUpload/index')
+
     } catch (error) {
         next(error)
     }
