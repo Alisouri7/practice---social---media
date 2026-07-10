def59264c7f8b90111d413a8904bf3ed6519b1a8
Ali Souri <souri7205@gmail.com>
2026-07-10 16:15:00 +0330

    ollower and following mechanism

--- src/modules/pages/controller/page.js
+++ src/modules/pages/controller/page.js
@@ -1,21 +1,45 @@
 const hasAccessToPage = require("./../../../utils/hasAccessToPage");
-
+const followModel = require('./../../follower/model/Follower');
 
 exports.getPage = async (req, res, next) => {
     try {
         const userID = req.user._id.toString();
-        
+
         const { pageID } = req.params;
 
         const hasAccess = await hasAccessToPage(userID, pageID);
-        
+
+        const followStatus = await followModel.findOne({ follower: userID, following: pageID }).lean();
+
         if (!hasAccess) {
             req.flash('error', 'Follow Page To Show Content')
-            return res.render('./index')
+            return res.render('./Pages/Profiles/index', {
+                followStatus: Boolean(followStatus),
+                pageID
+            })
         };
 
-        res.render('./Pages/Profiles/index')
+        res.render('./Pages/Profiles/index', {
+            followStatus: Boolean(followStatus),
+            pageID
+        })
     } catch (err) {
         next(err)
+    }
+}
+
+exports.follow = async (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+}
+
+exports.unFollow = async (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        next(error)
     }
 }
--- src/modules/pages/routes/router.js
+++ src/modules/pages/routes/router.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const pageCpntroller = require('./../controller/page');
 const authMiddleware = require('./../../../utils/middlewares/auth');
 
-router.route('/:pageID').get(authMiddleware, pageCpntroller.getPage)
+router.route('/:pageID').get(authMiddleware, pageCpntroller.getPage);
+router.route('/:pageID/follow').post(authMiddleware, pageCpntroller.follow);
+router.route('/:pageID/unfollow').post(authMiddleware, pageCpntroller.unFollow);
 
 module.exports = router;
