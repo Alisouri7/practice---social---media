e787647b377de023bebd057d1a2a0b4a07ead5e0
Ali Souri <souri7205@gmail.com>
2026-07-27 11:14:26 +0330

    comment model

---
+++ src/modules/comments/model/Comment.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const schema = mongoose.Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    post: {
+        type: mongoose.Types.ObjectId,
+        ref: 'Post',
+        required: true
+    },
+    user: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    parent: {
+        type: mongoose.Types.ObjectId,
+        ref: "Comment"
+    }
+}, { timestamps: true });
+
+const model = mongoose.model('Comment', schema);
+
+module.exports = model;
